9b8dd1edd215ad58a5fe5e568e6e77d3abe7c0a8
Jeff Triplett <jeff.triplett@gmail.com>
2013-07-10 21:44:42 -0500

    PEP-8

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     long_description=open('README.txt').read(),
     install_requires=[
         "Django >= 1.4.1",
-        "redis >= 2.7.6" 
+        "redis >= 2.7.6"
     ],
     url='https://github.com/SawdustSoftware/disredis',
     zip_safe=False
